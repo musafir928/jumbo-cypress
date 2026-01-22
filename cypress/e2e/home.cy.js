d1955760c4d2b5ae24446c04a9bc10d464f8c87d
musafir928 <uganoghli@gmail.com>
2026-01-22 15:59:47 +0100

    add home page navigation tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,30 @@
-/// <reference types="cypress" />
+import HomePage from '../pages/HomePage'
 
-it("should visit jumbo home page", () => {
-    cy.visit("/");
-    cy.title().should('include', 'Jumbo')
+describe('Home Page Navigation - Jumbo', () => {
+    const homePage = new HomePage()
+
+    beforeEach(() => {
+        homePage.visit()
+    })
+
+    it('should display all main navigation options', () => {
+        homePage.verifyPageTitle()
+        homePage.verifyNavigationVisible()
+    })
+
+    it('should navigate to Producten page', () => {
+        homePage.goToProducten()
+    })
+
+    it('should navigate to Recepten page', () => {
+        homePage.goToRecepten()
+    })
+
+    it('should navigate to Aanbiedingen page', () => {
+        homePage.goToAanbiedingen()
+    })
+
+    it('should navigate to Mandje page', () => {
+        homePage.goToMandje()
+    })
 })
--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -1,1 +1,67 @@
-// to be implemented
+function acceptCookiesIfPresent() {
+    cy.get('body').then($body => {
+        if ($body.find('#onetrust-accept-btn-handler').length) {
+            cy.get('#onetrust-accept-btn-handler').click()
+        }
+    })
+}
+
+class HomePage {
+    visit() {
+        cy.visit('/')
+        acceptCookiesIfPresent();
+    }
+
+    productenLink() {
+        return cy.contains('a', 'Producten')
+    }
+
+    receptenLink() {
+        return cy.contains('a', 'Recepten')
+    }
+
+    aanbiedingenLink() {
+        return cy.contains('a', 'Aanbiedingen')
+    }
+
+    mandjeIcon() {
+        return cy.get(`[data-testid="basket-button"]`)
+    }
+
+    // --- Assertions ---
+    verifyPageTitle() {
+        cy.title().should('match', /jumbo/i)
+    }
+
+    verifyNavigationVisible() {
+        this.productenLink().should('be.visible')
+        this.receptenLink().should('be.visible')
+        this.aanbiedingenLink().should('be.visible')
+        this.mandjeIcon().should('be.visible')
+    }
+
+    goToProducten() {
+        this.productenLink().click()
+        cy.get('[data-testid="products-title"]')
+            .should('be.visible')
+            .and('contain.text', 'Producten')
+    }
+
+    goToRecepten() {
+        this.receptenLink().click()
+        cy.location('pathname').should('include', '/recepten')
+    }
+
+    goToAanbiedingen() {
+        this.aanbiedingenLink().click()
+        cy.location('pathname').should('include', '/aanbiedingen')
+    }
+
+    goToMandje() {
+        this.mandjeIcon().click()
+        cy.get('.basket-page');
+    }
+
+}
+
+export default HomePage
